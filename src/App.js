e80f8f02ee2a110e2aa71eae52ba99239d096fea
NaveenShukla1 <nshukla1045@gmail.com>
2023-02-05 12:56:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alerts from './components/Alerts';
+import React, { useState } from 'react'
+import AboutUs from './components/AboutUs'
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
 function App() {
+
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message) =>{
+    setAlert({
+      msg: message,
+      // type: type
+    })
+
+    setTimeout(() =>{
+      setAlert(null);
+    }, 1500)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      
+      <Navbar title="TextUtils"  />
+      <Alerts alert={alert}/>
+      <div className="container mx-11 my-5">
+
+        <Routes>
+          <Route exact path="/" element={<TextForm showAlert={showAlert} heading= "Enter Your Text Here"/>}/>
+          <Route exact path="/about" element={<AboutUs />}/>
+          {/* <Route path="*" element={<NotFound/>}/> */}
+        </Routes>
+
+
+      </div> 
+
+      </Router>
+    </>
   );
 }
 
